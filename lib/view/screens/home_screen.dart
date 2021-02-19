655dc075cc951416d8153425d5630232fa81f965
i-brain <manqanat1@gmail.com>
2021-02-19 10:35:07 +0400

    first commit

--- lib/view/screens/home_screen.dart
+++ lib/view/screens/home_screen.dart
@@ -2,29 +2,45 @@ import 'package:TaskApp/controller/card_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+const Color blue = Color.fromRGBO(1, 118, 228, 1);
+
 class HomeScreen extends StatelessWidget {
   final CardController controller = Get.put(CardController());
 
-  AppBar buildAppbar() {
-    return AppBar(
-      elevation: 0.0,
-      centerTitle: true,
-      iconTheme: IconThemeData(color: Colors.blue),
-      title: Text(
-        'task.',
-        style: TextStyle(color: Colors.blue, fontWeight: FontWeight.bold),
+  Widget buildAppbar() {
+    return PreferredSize(
+      preferredSize: Size(double.infinity, 66),
+      child: AppBar(
+        elevation: 0.0,
+        centerTitle: true,
+        leading: Icon(
+          Icons.menu,
+          color: blue,
+          size: 35,
+        ),
+        title: Text(
+          'task.',
+          style:
+              TextStyle(fontSize: 22, color: blue, fontWeight: FontWeight.w900),
+        ),
       ),
     );
   }
 
-  FloatingActionButton buildFAT() {
-    return FloatingActionButton(
-      child: Icon(
-        Icons.add,
-        size: 30,
-        color: Colors.blue,
+  Widget buildFAT() {
+    return Container(
+      height: 53,
+      width: 53,
+      child: FittedBox(
+        child: FloatingActionButton(
+          child: Icon(
+            Icons.add,
+            size: 38,
+            color: blue,
+          ),
+          onPressed: () {},
+        ),
       ),
-      onPressed: () {},
     );
   }
 
@@ -33,26 +49,28 @@ class HomeScreen extends StatelessWidget {
     return SafeArea(
       child: Scaffold(
         backgroundColor: Colors.white,
-        drawer: Drawer(
-          child: ListView(
-            children: [DrawerHeader(child: Text('Drawer'))],
-          ),
-        ),
         appBar: buildAppbar(),
         floatingActionButton: buildFAT(),
         body: GetX<CardController>(
           initState: (state) => controller.getData(),
-          builder: (controller) {
+          builder: (_) {
             if (controller.isDataLoading.value)
-              return Center(child: CircularProgressIndicator());
+              return Center(
+                child: CircularProgressIndicator(
+                  valueColor: AlwaysStoppedAnimation<Color>(blue),
+                ),
+              );
             return Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Padding(
-                  padding: const EdgeInsets.all(15),
+                  padding: const EdgeInsets.only(left: 16, bottom: 12),
                   child: Text(
                     'Tasks',
-                    style: TextStyle(fontSize: 20),
+                    style: TextStyle(
+                        color: Color.fromRGBO(51, 50, 50, 1),
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500),
                   ),
                 ),
                 Expanded(
--- lib/view/widgets/list_card.dart
+++ lib/view/widgets/list_card.dart
@@ -1,48 +1,70 @@
 import 'package:TaskApp/model/my_card.dart';
-import 'package:circular_check_box/circular_check_box.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class ListCard extends StatelessWidget {
   final MyCard card;
 
-  final RxBool isChecked = false.obs;
+  final RxBool isCircleChecked = false.obs;
 
-  final List colors = [Colors.red[100], Colors.blue[100], Colors.green[100]];
+  final List colors = [
+    Color.fromRGBO(255, 222, 222, 1),
+    Color.fromRGBO(91, 177, 255, 1),
+    Color.fromRGBO(177, 248, 193, 1)
+  ];
+
+  /*
+  Qeyd: Listtile-ın enini dizaynda göstərildiyi kimi verdikdə bəzi telefonlarda 
+        pixel xetası verəcəyini düşündüyüm üçün Expanded-lə etməyi üstün tutdum.
+  */
 
   ListCard({this.card});
   @override
   Widget build(BuildContext context) {
     return IntrinsicHeight(
       child: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+        padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 14),
         child: Row(
           children: [
             Expanded(
               flex: 1,
               child: Container(
-                margin: EdgeInsets.only(right: 5, left: 2),
+                margin: EdgeInsets.only(right: 6.16, left: 2),
                 decoration: BoxDecoration(
                     color: colors[card.id - 1],
-                    borderRadius: BorderRadius.circular(10)),
+                    borderRadius: BorderRadius.circular(6)),
               ),
             ),
             Expanded(
               flex: 19,
               child: Container(
+                height: 66,
                 decoration: BoxDecoration(
                   color: Color.fromRGBO(248, 248, 248, 1),
-                  borderRadius: BorderRadius.circular(10),
+                  borderRadius: BorderRadius.circular(12),
                 ),
-                child: ListTile(
-                  title: Text(card.title),
-                  subtitle: Text(card.timestamp),
-                  trailing: Obx(
-                    () => CircularCheckBox(
-                      inactiveColor: Colors.blue,
-                      activeColor: Colors.blue,
-                      value: isChecked.value,
-                      onChanged: (_) => isChecked.value = !isChecked.value,
+                child: Obx(
+                  () => ListTile(
+                    onTap: isCircleChecked.toggle,
+                    title: Text(
+                      card.title,
+                      style:
+                          TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+                    ),
+                    subtitle: Text(
+                      card.timestamp,
+                      style:
+                          TextStyle(fontSize: 12, fontWeight: FontWeight.w400),
+                    ),
+                    trailing: Container(
+                      height: 18,
+                      width: 18,
+                      decoration: BoxDecoration(
+                          border: Border.all(color: Colors.blue, width: 2),
+                          color: isCircleChecked.value
+                              ? Colors.blue
+                              : Colors.white,
+                          shape: BoxShape.circle),
                     ),
                   ),
                 ),
